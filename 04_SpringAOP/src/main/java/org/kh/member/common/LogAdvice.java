02bd93d73b2998198ea8b07b8121560bd1cb41ac
user1-PC\user1 <changho93@naver.com>
2018-07-06 16:18:54 +0900

    g

--- 04_SpringAOP/src/main/java/org/kh/member/common/LogAdvice.java
+++ 04_SpringAOP/src/main/java/org/kh/member/common/LogAdvice.java
@@ -4,4 +4,8 @@ public class LogAdvice {
 	public void printLog() {
 		System.out.println("[공통 로그 - LogAdvice] : 비즈니스 로직 수행 전 로그 기록 입니다.");
 	}
+	
+	public void printTransactionLog() {
+		System.out.println("[트랜잭션 로그 - LogAdvice] : 트랜잭션 처리 로그");
+	}
 }
