28124cfb762ed721a1bf735c4d50e8b928f60ef1
user1-PC\user1 <changho93@naver.com>
2018-07-12 21:14:21 +0900

    zhzh

---
+++ PaPaGoTest/src/test/TestMain.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.Scanner;
+
+public class TestMain {
+	
+	public static void main(String[] args) {
+		PaPaGo ppg = new PaPaGo();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("번역할 문장을 입력하세요 : ");
+		
+		String data = sc.nextLine();
+		
+		System.out.println("번역 결과 : " + ppg.testPaPaGo(data));
+		
+	}
+}
